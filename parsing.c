72c999248bdefd0d965ac457d0794575b8f4bb6e
Tony Roman <toroman@c2r12p1.42nice.fr>
2024-12-28 15:17:25 +0100

    push

--- parsing.c
+++ parsing.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tony <tony@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: toroman <toroman@student.42nice.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/26 11:51:52 by toroman           #+#    #+#             */
-/*   Updated: 2024/12/28 15:07:47 by tony             ###   ########.fr       */
+/*   Updated: 2024/12/28 15:17:00 by toroman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,7 +78,7 @@ void	checkrectangle(t_map *copie)
 	{
 		if (ft_strlen(copie->map[i]) != countligne)
 		{
-			ft_printf("tony la caille");
+			ft_printf("Error:\nthe map is not rectangular");
 			exit(EXIT_FAILURE);
 		}
 		i++;
